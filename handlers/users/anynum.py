7b6f7c4eda4dd73c198398da1baea567be8af63b
mwh4t <korzunov.matvey@yandex.ru>
2023-04-04 18:55:22 +0300

    changes in buttons and anynum

--- handlers/users/anynum.py
+++ handlers/users/anynum.py
@@ -7,23 +7,40 @@ from datetime import datetime
 # handler будет запущен, если сообщение содержит только цифры
 @dp.message_handler(lambda message: message.text.isdigit())
 async def any_number(message: types.Message):
-    try:
-        date = datetime.now()
-        link = f"https://mtkspb.ru/public/educational/schedule/Расписание%20{message.text:0>2}-{date.month:0>2}-{date.year}.pdf"
-        response = requests.get(link) # отправка HTTP GET запроса на сайт
+    date = datetime.now()
+    link1 = f"https://mtkspb.ru/public/educational/schedule/Расписание%20{message.text:0>2}.{date.month:0>2}.{date.year}.pdf"
+    link2 = f"https://mtkspb.ru/public/educational/schedule/Расписание%20{message.text:0>2}-{date.month:0>2}-{date.year}.pdf"
+    response1 = requests.get(link1) # отправка HTTP GET запроса на сайт
+    response2 = requests.get(link2)  # отправка HTTP GET запроса на сайт
+    if response1.status_code == 200 and response2.status_code == 200:
         # полученные данные сохраняются в PDF
         pdf = open("pdf.pdf", 'wb')
-        pdf.write(response.content)
+        pdf.write(response2.content)
         pdf.close()
-        file_path = "pdf.pdf" # путь к PDF файлу
+        file_path = "pdf.pdf"  # путь к PDF файлу
         # дефолтные настройки
         dpi = 300
         zoom = dpi / 72
         magnify = fitz.Matrix(zoom, zoom)
         page = fitz.open(file_path)
-        pix = page[0].get_pixmap(matrix=magnify) # 1-ая страница PDF преобразуется в пиксельную карту
-        pix.save("page.png") # пиксельная карта сохраняется в формате .png
+        pix = page[0].get_pixmap(matrix=magnify)  # 1-ая страница PDF преобразуется в пиксельную карту
+        pix.save("page.png")  # пиксельная карта сохраняется в формате .png
         img = open("page.png", 'rb')
         await message.answer_photo(photo=img, caption='@mtkspbbot')
-    except:
+    elif response1.status_code == 200 and response2.status_code == 404:
+        # полученные данные сохраняются в PDF
+        pdf = open("pdf.pdf", 'wb')
+        pdf.write(response1.content)
+        pdf.close()
+        file_path = "pdf.pdf"  # путь к PDF файлу
+        # дефолтные настройки
+        dpi = 300
+        zoom = dpi / 72
+        magnify = fitz.Matrix(zoom, zoom)
+        page = fitz.open(file_path)
+        pix = page[0].get_pixmap(matrix=magnify)  # 1-ая страница PDF преобразуется в пиксельную карту
+        pix.save("page.png")  # пиксельная карта сохраняется в формате .png
+        img = open("page.png", 'rb')
+        await message.answer_photo(photo=img, caption='@mtkspbbot')
+    else:
         await message.answer('Ничего не найдено!')
--- handlers/users/buttons.py
+++ handlers/users/buttons.py
@@ -6,13 +6,16 @@ from datetime import datetime, timedelta
 
 @dp.message_handler(text='Вчера')
 async def btn_yesterday(message: types.Message):
-    try:
-        date = str(datetime.strftime(datetime.now() - timedelta(1), '%d-%m-%Y'))
-        link = f"https://mtkspb.ru/public/educational/schedule/Расписание%20{date}.pdf"
-        response = requests.get(link) # отправка HTTP GET запроса на сайт
+    date1 = str(datetime.strftime(datetime.now() - timedelta(1), '%d.%m.%Y'))
+    date2 = str(datetime.strftime(datetime.now() - timedelta(1), '%d-%m-%Y'))
+    link1 = f"https://mtkspb.ru/public/educational/schedule/Расписание%20{date1}.pdf"
+    link2 = f"https://mtkspb.ru/public/educational/schedule/Расписание%20{date2}.pdf"
+    response1 = requests.get(link1) # отправка HTTP GET запроса на сайт
+    response2 = requests.get(link2)  # отправка HTTP GET запроса на сайт
+    if response1.status_code == 200 and response2.status_code == 200:
         # полученные данные сохраняются в PDF
         pdf = open("pdf.pdf", 'wb')
-        pdf.write(response.content)
+        pdf.write(response2.content)
         pdf.close()
         file_path = "pdf.pdf" # путь к PDF файлу
         # дефолтные настройки
@@ -24,18 +27,36 @@ async def btn_yesterday(message: types.Message):
         pix.save("page.png") # пиксельная карта сохраняется в формате .png
         img = open("page.png", 'rb')
         await message.answer_photo(photo=img, caption='@mtkspbbot')
-    except:
+    elif response1.status_code == 200 and response2.status_code == 404:
+        # полученные данные сохраняются в PDF
+        pdf = open("pdf.pdf", 'wb')
+        pdf.write(response1.content)
+        pdf.close()
+        file_path = "pdf.pdf"  # путь к PDF файлу
+        # дефолтные настройки
+        dpi = 300
+        zoom = dpi / 72
+        magnify = fitz.Matrix(zoom, zoom)
+        page = fitz.open(file_path)
+        pix = page[0].get_pixmap(matrix=magnify)  # 1-ая страница PDF преобразуется в пиксельную карту
+        pix.save("page.png")  # пиксельная карта сохраняется в формате .png
+        img = open("page.png", 'rb')
+        await message.answer_photo(photo=img, caption='@mtkspbbot')
+    else:
         await message.answer('Ничего не найдено!')
 
 @dp.message_handler(text='Сегодня')
 async def btn_today(message: types.Message):
-    try:
-        date = str(datetime.strftime(datetime.now(), '%d-%m-%Y'))
-        link = f"https://mtkspb.ru/public/educational/schedule/Расписание%20{date}.pdf"
-        response = requests.get(link) # отправка HTTP GET запроса на сайт
+    date1 = str(datetime.strftime(datetime.now(), '%d.%m.%Y'))
+    date2 = str(datetime.strftime(datetime.now(), '%d-%m-%Y'))
+    link1 = f"https://mtkspb.ru/public/educational/schedule/Расписание%20{date1}.pdf"
+    link2 = f"https://mtkspb.ru/public/educational/schedule/Расписание%20{date2}.pdf"
+    response1 = requests.get(link1) # отправка HTTP GET запроса на сайт
+    response2 = requests.get(link2)  # отправка HTTP GET запроса на сайт
+    if response1.status_code == 200 and response2.status_code == 200:
         # полученные данные сохраняются в PDF
         pdf = open("pdf.pdf", 'wb')
-        pdf.write(response.content)
+        pdf.write(response2.content)
         pdf.close()
         file_path = "pdf.pdf" # путь к PDF файлу
         # дефолтные настройки
@@ -47,18 +68,36 @@ async def btn_today(message: types.Message):
         pix.save("page.png") # пиксельная карта сохраняется в формате .png
         img = open("page.png", 'rb')
         await message.answer_photo(photo=img, caption='@mtkspbbot')
-    except:
+    elif response1.status_code == 200 and response2.status_code == 404:
+        # полученные данные сохраняются в PDF
+        pdf = open("pdf.pdf", 'wb')
+        pdf.write(response1.content)
+        pdf.close()
+        file_path = "pdf.pdf"  # путь к PDF файлу
+        # дефолтные настройки
+        dpi = 300
+        zoom = dpi / 72
+        magnify = fitz.Matrix(zoom, zoom)
+        page = fitz.open(file_path)
+        pix = page[0].get_pixmap(matrix=magnify)  # 1-ая страница PDF преобразуется в пиксельную карту
+        pix.save("page.png")  # пиксельная карта сохраняется в формате .png
+        img = open("page.png", 'rb')
+        await message.answer_photo(photo=img, caption='@mtkspbbot')
+    else:
         await message.answer('Ничего не найдено!')
 
 @dp.message_handler(text='Завтра')
 async def btn_tomorrow(message: types.Message):
-    try:
-        date = str(datetime.strftime(datetime.now() + timedelta(1), '%d-%m-%Y'))
-        link = f"https://mtkspb.ru/public/educational/schedule/Расписание%20{date}.pdf"
-        response = requests.get(link) # отправка HTTP GET запроса на сайт
+    date1 = str(datetime.strftime(datetime.now() + timedelta(1), '%d.%m.%Y'))
+    date2 = str(datetime.strftime(datetime.now() + timedelta(1), '%d-%m-%Y'))
+    link1 = f"https://mtkspb.ru/public/educational/schedule/Расписание%20{date1}.pdf"
+    link2 = f"https://mtkspb.ru/public/educational/schedule/Расписание%20{date2}.pdf"
+    response1 = requests.get(link1) # отправка HTTP GET запроса на сайт
+    response2 = requests.get(link2)  # отправка HTTP GET запроса на сайт
+    if response1.status_code == 200 and response2.status_code == 200:
         # полученные данные сохраняются в PDF
         pdf = open("pdf.pdf", 'wb')
-        pdf.write(response.content)
+        pdf.write(response2.content)
         pdf.close()
         file_path = "pdf.pdf" # путь к PDF файлу
         # дефолтные настройки
@@ -70,5 +109,20 @@ async def btn_tomorrow(message: types.Message):
         pix.save("page.png") # пиксельная карта сохраняется в формате .png
         img = open("page.png", 'rb')
         await message.answer_photo(photo=img, caption='@mtkspbbot')
-    except:
+    elif response1.status_code == 200 and response2.status_code == 404:
+        # полученные данные сохраняются в PDF
+        pdf = open("pdf.pdf", 'wb')
+        pdf.write(response1.content)
+        pdf.close()
+        file_path = "pdf.pdf"  # путь к PDF файлу
+        # дефолтные настройки
+        dpi = 300
+        zoom = dpi / 72
+        magnify = fitz.Matrix(zoom, zoom)
+        page = fitz.open(file_path)
+        pix = page[0].get_pixmap(matrix=magnify)  # 1-ая страница PDF преобразуется в пиксельную карту
+        pix.save("page.png")  # пиксельная карта сохраняется в формате .png
+        img = open("page.png", 'rb')
+        await message.answer_photo(photo=img, caption='@mtkspbbot')
+    else:
         await message.answer('Ничего не найдено!')
