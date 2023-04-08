23b2a5470d8280e3075d8dca999f1c7d9d19a668
mwh4t <korzunov.matvey@yandex.ru>
2023-04-08 23:59:12 +0300

    changes to buttons and anynum

--- handlers/users/anynum.py
+++ handlers/users/anynum.py
@@ -8,11 +8,15 @@ from datetime import datetime
 @dp.message_handler(lambda message: message.text.isdigit())
 async def any_number(message: types.Message):
     date = datetime.now()
-    link1 = f"https://mtkspb.ru/public/educational/schedule/Расписание%20{message.text:0>2}.{date.month:0>2}.{date.year}.pdf"
-    link2 = f"https://mtkspb.ru/public/educational/schedule/Расписание%20{message.text:0>2}-{date.month:0>2}-{date.year}.pdf"
+    link1 = f"https://mtkspb.ru/public/educational/Расписание%20{message.text:0>2}.{date.month:0>2}.{date.year}.pdf"
+    link2 = f"https://mtkspb.ru/public/educational/Расписание%20{message.text:0>2}-{date.month:0>2}-{date.year}.pdf"
+    link3 = f"https://mtkspb.ru/public/educational/schedule/Расписание%20{message.text:0>2}.{date.month:0>2}.{date.year}.pdf"
+    link4 = f"https://mtkspb.ru/public/educational/schedule/Расписание%20{message.text:0>2}-{date.month:0>2}-{date.year}.pdf"
     response1 = requests.get(link1) # отправка HTTP GET запроса на сайт
-    response2 = requests.get(link2)  # отправка HTTP GET запроса на сайт
-    if response1.status_code == 200 and response2.status_code == 200:
+    response2 = requests.get(link2) # отправка HTTP GET запроса на сайт
+    response3 = requests.get(link3) # отправка HTTP GET запроса на сайт
+    response4 = requests.get(link4) # отправка HTTP GET запроса на сайт
+    if response1.status_code == 200 and response2.status_code == 200 and response3.status_code == 404 and response4.status_code == 404:
         # полученные данные сохраняются в PDF
         pdf = open("pdf.pdf", 'wb')
         pdf.write(response2.content)
@@ -27,10 +31,40 @@ async def any_number(message: types.Message):
         pix.save("page.png")  # пиксельная карта сохраняется в формате .png
         img = open("page.png", 'rb')
         await message.answer_photo(photo=img, caption='@mtkspbbot')
-    elif response1.status_code == 200 and response2.status_code == 404:
+    elif response1.status_code == 200 and response2.status_code == 404 and response3.status_code == 404 and response4.status_code == 404:
         # полученные данные сохраняются в PDF
         pdf = open("pdf.pdf", 'wb')
         pdf.write(response1.content)
+        pdf.close()
+        file_path = "pdf.pdf"  # путь к PDF файлу
+        # дефолтные настройки
+        dpi = 300
+        zoom = dpi / 72
+        magnify = fitz.Matrix(zoom, zoom)
+        page = fitz.open(file_path)
+        pix = page[0].get_pixmap(matrix=magnify)  # 1-ая страница PDF преобразуется в пиксельную карту
+        pix.save("page.png")  # пиксельная карта сохраняется в формате .png
+        img = open("page.png", 'rb')
+        await message.answer_photo(photo=img, caption='@mtkspbbot')
+    elif response1.status_code == 404 and response2.status_code == 404 and response3.status_code == 200 and response4.status_code == 200:
+        # полученные данные сохраняются в PDF
+        pdf = open("pdf.pdf", 'wb')
+        pdf.write(response4.content)
+        pdf.close()
+        file_path = "pdf.pdf"  # путь к PDF файлу
+        # дефолтные настройки
+        dpi = 300
+        zoom = dpi / 72
+        magnify = fitz.Matrix(zoom, zoom)
+        page = fitz.open(file_path)
+        pix = page[0].get_pixmap(matrix=magnify)  # 1-ая страница PDF преобразуется в пиксельную карту
+        pix.save("page.png")  # пиксельная карта сохраняется в формате .png
+        img = open("page.png", 'rb')
+        await message.answer_photo(photo=img, caption='@mtkspbbot')
+    elif response1.status_code == 404 and response2.status_code == 404 and response3.status_code == 200 and response4.status_code == 404:
+        # полученные данные сохраняются в PDF
+        pdf = open("pdf.pdf", 'wb')
+        pdf.write(response3.content)
         pdf.close()
         file_path = "pdf.pdf"  # путь к PDF файлу
         # дефолтные настройки
