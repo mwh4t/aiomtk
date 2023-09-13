4a7ed209ff16726e22b0ace684ad78683d56c830
eldak evrohimov <korzunov.matvey@yandex.ru>
2023-09-13 20:36:48 +0300

    added a counter for mailing

--- handlers/users/admin/bot_mailing.py
+++ handlers/users/admin/bot_mailing.py
@@ -7,11 +7,13 @@ from utils.db_api import quick_commands as commands
 from states import bot_mailing
 from data.config import admins
 
+
 @dp.message_handler(text='/mailing', chat_id=admins)
 async def start_mailing(message: types.Message):
     await message.answer(f'Введите текст рассылки')
     # установление значения, используемое для отслеживания ввода рассылки
     await bot_mailing.text.set()
+
 
 @dp.message_handler(state=bot_mailing.text, chat_id=admins)
 async def mailing_text(message: types.Message, state: FSMContext):
@@ -24,34 +26,39 @@ async def mailing_text(message: types.Message, state: FSMContext):
                                           InlineKeyboardButton(text='Отмена', callback_data='quit')
                                       ]
                                   ])
-    await state.update_data(text=answer) # обновление состояния данными ответа
+    await state.update_data(text=answer)  # обновление состояния данными ответа
     await message.answer(text=answer, reply_markup=markup)
-    await bot_mailing.state.set() # установление состояния
+    await bot_mailing.state.set()  # установление состояния
+
 
 @dp.callback_query_handler(text='next', state=bot_mailing.state, chat_id=admins)
 async def start(call: types.CallbackQuery, state: FSMContext):
-    users = await commands.select_all_users() # извлечение пользователей из БД
+    users = await commands.select_all_users()  # извлечение пользователей из БД
     data = await state.get_data()
     text = data.get('text')
-    await state.finish() # завершение состояния
+    sent_messages_count = 0
+    await state.finish()  # завершение состояния
     for user in users:
         try:
             await dp.bot.send_message(chat_id=user.user_id, text=text)
-            await sleep(0.33) # задержка каждого сообщения
+            await sleep(0.33)  # задержка каждого сообщения
+            sent_messages_count += 1  # увеличение счётчика успешных сообщений
         except Exception:
             pass
-    await call.message.answer('Рассылка выполнена!')
+    await call.message.answer(f'Рассылка выполнена! Дошла до {sent_messages_count} человек.')
+
 
 @dp.callback_query_handler(text='add_photo', state=bot_mailing.state, chat_id=admins)
 async def add_photo(call: types.CallbackQuery):
     await call.message.edit_text(text='Пришлите фото')
-    await bot_mailing.photo.set() # установление состояния
+    await bot_mailing.photo.set()  # установление состояния
+
 
 @dp.message_handler(state=bot_mailing.photo, content_types=types.ContentType.PHOTO, chat_id=admins)
 async def mailing_text(message: types.Message, state: FSMContext):
-    photo_file_id = message.photo[-1].file_id # получение самой большой фото
-    await state.update_data(photo=photo_file_id) # обновление состояния
-    data = await state.get_data() # извлечение текущих данных
+    photo_file_id = message.photo[-1].file_id  # получение самой большой фото
+    await state.update_data(photo=photo_file_id)  # обновление состояния
+    data = await state.get_data()  # извлечение текущих данных
     text = data.get('text')
     photo = data.get('photo')
     markup = InlineKeyboardMarkup(row_width=2,
@@ -63,20 +70,24 @@ async def mailing_text(message: types.Message, state: FSMContext):
                                   ])
     await message.answer_photo(photo=photo, caption=text, reply_markup=markup)
 
+
 @dp.callback_query_handler(text='next', state=bot_mailing.photo, chat_id=admins)
 async def start(call: types.CallbackQuery, state: FSMContext):
-    users = await commands.select_all_users() # извлечение пользователей из БД
-    data = await state.get_data() # извлечение текущих данных
+    users = await commands.select_all_users()  # извлечение пользователей из БД
+    data = await state.get_data()  # извлечение текущих данных
     text = data.get('text')
     photo = data.get('photo')
-    await state.finish() # завершение состояния
+    sent_messages_count = 0
+    await state.finish()  # завершение состояния
     for user in users:
         try:
             await dp.bot.send_photo(chat_id=user.user_id, photo=photo, caption=text)
-            await sleep(0.33) # задержка каждого сообщения
+            await sleep(0.33)  # задержка каждого сообщения
+            sent_messages_count += 1  # увеличение счётчика успешных сообщений
         except Exception:
             pass
-    await call.message.answer('Рассылка выполнена!')
+    await call.message.answer(f'Рассылка выполнена! Дошла до {sent_messages_count} человек.')
+
 
 @dp.message_handler(state=bot_mailing.text, chat_id=admins)
 async def no_photo(message: types.Message):
@@ -88,7 +99,8 @@ async def no_photo(message: types.Message):
                                   ])
     await message.edit_text('Пришлите фото', reply_markup=markup)
 
+
 @dp.callback_query_handler(text='quit', state=[bot_mailing.text, bot_mailing.photo, bot_mailing.state], chat_id=admins)
 async def quit(call: types.CallbackQuery, state: FSMContext):
-    await state.finish() # завершение состояния
+    await state.finish()  # завершение состояния
     await call.message.answer('Рассылка отменена')
